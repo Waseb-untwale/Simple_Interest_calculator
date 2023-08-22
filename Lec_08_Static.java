83c1eaa191dabd8b2f8cef720bc70673ad92c778
Syed Minnatullah - Quadri <88645248+s-m-quadri@users.noreply.github.com>
2023-08-22 08:43:46 +0530

    Format and simplify variable names

--- Lec_08_Static.java
+++ Lec_08_Static.java
@@ -1,53 +1,44 @@
-            // ******* Bank of maharastra Loan system *******///
-
 import java.util.Scanner;
-class farmar{
-    int pa;
-    float td;
-    static float ri;
-    float si;
 
-    static{
-        ri=1.2f;
+class farmer {
+    int amount;
+    float time;
+    static float rate_of_interest;
+    float simple_interest;
+
+    static {
+        rate_of_interest = 1.2f;
     }
-    
 
+    void input() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Principal Amount: ");
+        amount = scan.nextInt();
+        System.out.println("Enter Time Duration: ");
+        time = scan.nextFloat();
 
-void input(){
-    Scanner scan = new Scanner(System.in);
-    System.out.println("please Enter the amount required");
-    pa=scan.nextInt();
-    System.out.println("please enter the time duration(please enter your monthly time duration) for loan system ");
-    td=scan.nextFloat();
+    }
 
+    void compute() {
+        simple_interest = (amount * time * rate_of_interest) / 100f;
+    }
 
+    void display() {
+        System.out.println("> Simple Interest: " + simple_interest);
+    }
 }
-
-void compute(){
-    si=(pa*td*ri)/100f;
-}
-
-void disp(){
-    System.out.println("Simple Interest of required amount is :"+si);
-}
-}
-
-
-
 
 public class Lec_08_Static {
-public static void main(String[] args){
-farmar f1 = new farmar();
-farmar f2 = new farmar();
+    public static void main(String[] args) {
+        farmer f1 = new farmer();
+        farmer f2 = new farmer();
 
-f1.input();
-f1.compute();
-f1.disp();
+        f1.input();
+        f1.compute();
+        f1.display();
 
-f2.input();
-f2.compute();
-f2.disp();
-
-    
-}
+        f2.input();
+        f2.compute();
+        f2.display();
+    }
 }
